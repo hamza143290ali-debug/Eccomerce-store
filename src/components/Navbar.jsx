b7e58864a6d6ccaf5c866d485851d7f2274ac27c
HAMZAALI314 <hamzaali314@gmail.com>
2026-08-01 19:29:38 +0500

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   const { cartCount } = useCart();
   return (
     <>
-      <nav className="w-full h-15  bg-gray-950/95 flex justify-between border-b-1 border-orange-500 items-center sticky md:pt-5 md:pb-5 ">
+      <nav className="w-full h-20  bg-gray-950/95 flex justify-between border-b-1 border-orange-500 items-center sticky md:pt-5 md:pb-5 ">
         {/* left side */}
         <div className="flex space-x-1 ml-5 text-2xl font-extrabold items-center md:text-4xl md:font-extrabold">
           <Link to="/">
@@ -16,7 +16,7 @@ function Navbar() {
               <House className="text-orange-400 curstor-pointer hover:text-orange-500 transition-all duration-200" />
             </span>
           </Link>
-          <h1>
+          <h1 className="lg:text-4xl lg:font-extrabold">
             HAMZA<span className="text-orange-500">STORE</span>
           </h1>
         </div>
